d1eed9f8146227bc0bffe90d7394335ac12b95b6
Midjord <midjord.andreas@gmail.com>
2021-12-01 07:03:32 +0100

    Day1 as one-liners

--- Day1.py
+++ Day1.py
@@ -5,31 +5,11 @@ numbers = list(map(int, input))
 
 
 def simpleIncreases(inputNumbers):
-    init = 0
-    count = 0
-
-    for i in inputNumbers[1:]:
-        if i > init:
-            count += 1
-
-        init = i
-
-    return count
+    return sum([inputNumbers[i] < inputNumbers[i+1] for i in range(len(inputNumbers)-1)])
 
 
 def rollingSumIncreases(inputNumbers):
-    rollingSum = inputNumbers[0] + inputNumbers[1] + inputNumbers[2]
-    count = 0
-
-    for i in range(len(inputNumbers)-3):
-        newSum = inputNumbers[i + 1] + inputNumbers[i + 2] + inputNumbers[i + 3]
-
-        if newSum > rollingSum:
-            count += 1
-
-        rollingSum = newSum
-
-    return count
+    return sum([inputNumbers[i] < inputNumbers[i+3] for i in range(len(inputNumbers)-3)])
 
 
 simpleIncreases = simpleIncreases(numbers)
