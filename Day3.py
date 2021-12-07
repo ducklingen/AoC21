ac491076ec4fef41c8db8d04fa2de26cfb7605c9
Midjord <midjord.andreas@gmail.com>
2021-12-07 15:56:19 +0100

    Day 3 - minor cleanup

--- Day3.py
+++ Day3.py
@@ -4,8 +4,6 @@ input = AoCHelper.read_input_lines("day3/input1.txt")
 input_lenght = len(input[0])
 gamma, epsilon = '', ''
 
-
-
 for i in range(input_lenght):
     zeros, ones = 0, 0
 
@@ -22,11 +20,14 @@ for i in range(input_lenght):
         gamma += '1'
         epsilon += '0'
 
+
+print(int(gamma, 2) * int(epsilon, 2))
+
 oxy, co2 = '', ''
 filtered_lines_oxy = input.copy()
 filtered_lines_c02 = input.copy()
 pos = 0
-while(len(filtered_lines_oxy) > 1):
+while len(filtered_lines_oxy) > 1:
 
     ones = sum([int(binary[pos]) for binary in filtered_lines_oxy])
 
@@ -41,7 +42,7 @@ oxy = filtered_lines_oxy[0]
 print(oxy)
 
 pos = 0
-while(len(filtered_lines_c02) > 1):
+while len(filtered_lines_c02) > 1:
 
     ones = sum([int(binary[pos]) for binary in filtered_lines_c02])
 
