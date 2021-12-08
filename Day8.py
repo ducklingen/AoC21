4469911f025ce7b4cc15e13a9013fa3580284679
Midjord <midjord.andreas@gmail.com>
2021-12-08 06:59:33 +0100

    Day 8 - initial solution

---
+++ Day8.py
@@ -0,0 +1,65 @@
+from helpers import AoCHelper
+
+input_data = AoCHelper.read_input_lines("day8/input1.txt")
+
+easy_digits_dict = {2: '1', 3: '7', 4: '4', 7: '8'}
+
+number_of_easy_knowns = 0
+sum_of_output = 0
+
+for line in input_data:
+    input, output = line.split('|')
+
+    input_values = input.split(' ')
+    output_values = output.split(' ')
+    input_values.sort(reverse=False, key=len)
+
+    output_number = ''
+    one_lines = []
+    nine_lines = []
+    three_lines = []
+
+    # Digit analysis
+    for i in input_values:
+        if i == '':
+            continue
+
+        if len(i) == 2:
+            one_lines = list(i)
+
+        if len(i) == 5 and min(k in list(i) for k in one_lines) == 1:
+            three_lines = list(i)
+
+        if len(i) == 6 and min(k in list(i) for k in three_lines) == 1:
+            nine_lines = list(i)
+
+    for o in output_values:
+        if o == '':
+            continue
+
+        if len(o) in easy_digits_dict.keys():
+            number_of_easy_knowns += 1
+            output_number += easy_digits_dict[len(o)]
+
+        if len(o) == 5:
+            if min(k in list(o) for k in one_lines) == 1:
+                output_number += '3'
+            elif min(k in nine_lines for k in o) == 1:
+                output_number += '5'
+            else:
+                output_number += '2'
+        if len(o) == 6:
+            if min(k in list(o) for k in three_lines) == 1:
+                output_number += '9'
+            elif min(k in list(o) for k in one_lines) == 1:
+                output_number += '0'
+            else:
+                output_number += '6'
+
+    sum_of_output += int(output_number)
+
+assert number_of_easy_knowns == 473
+print(f"Part 1: {number_of_easy_knowns}")
+
+assert sum_of_output == 1097568
+print(f"Part 2: {sum_of_output}")
