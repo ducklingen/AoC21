c15213a33b108083623931d853971b798f04d064
Midjord <midjord.andreas@gmail.com>
2021-12-09 07:27:13 +0100

    Day 9 - further cleanup

--- Day9.py
+++ Day9.py
@@ -1,7 +1,7 @@
 from helpers import AoCHelper, GlobalVariables
 
 input_lines = AoCHelper.read_input_lines("day9/input1.txt")
-grid = [list(map(int, list(line))) for line in input_lines]
+input_grid = [list(map(int, list(line))) for line in input_lines]
 
 
 def get_low_points(grid):
@@ -18,8 +18,8 @@ def get_low_points(grid):
 
 
 def get_basin(i, j, grid, precursors):
-    basin = [(i,j)]
-    precursors.append((i,j))
+    basin = [(i, j)]
+    precursors.append((i, j))
 
     for x, y in GlobalVariables.cardinal_directions:
         if 0 <= i + x < len(grid) and 0 <= j + y < len(grid[0]):
@@ -29,15 +29,15 @@ def get_basin(i, j, grid, precursors):
     return basin
 
 
-low_points = get_low_points(grid)
+low_points = get_low_points(input_grid)
 basins = []
 
-for lp in low_points:
-    basins.append(get_basin(lp[0], lp[1], grid, []))
+for x, y in low_points:
+    basins.append(get_basin(x, y, input_grid, []))
 
 basins.sort(reverse=True, key=len)
 
-low_points_sum = sum(grid[p[0]][p[1]] + 1 for p in low_points)
+low_points_sum = sum(input_grid[x][y] + 1 for x, y in low_points)
 assert low_points_sum == 524
 print(f"Part 1: {low_points_sum}")
 
