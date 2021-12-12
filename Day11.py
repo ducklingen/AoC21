493a574551e7eaa845b51f08e87c0cc91f5bb73c
Midjord <midjord.andreas@gmail.com>
2021-12-12 12:13:40 +0100

    Day 11 - better loops

--- Day11.py
+++ Day11.py
@@ -1,4 +1,4 @@
-from itertools import combinations_with_replacement
+from itertools import product
 
 from helpers.GlobalVariables import all_directions
 from helpers import AoCHelper
@@ -12,25 +12,24 @@ flashes = 0
 while True:
     step += 1
 
-    for x, y in AoCHelper.get_all_coordinates_in_grid(10, 10):
+    for x, y in product(range(10), range(10)):
         grid[x][y] = grid[x][y] + 1
 
-    while len([(x, y) for (x, y) in AoCHelper.get_all_coordinates_in_grid(10, 10) if grid[x][y] > 9]) > 0:
-        for x, y in AoCHelper.get_all_coordinates_in_grid(10, 10):
+    while len([(x, y) for x, y in product(range(10), range(10)) if grid[x][y] > 9]) > 0:
+        for x, y in product(range(10), range(10)):
             if grid[x][y] > 9:
                 for n, m in all_directions:
-                    if 0 <= x + n < len(grid) and 0 <= y + m < len(grid[0]):
-                        if 0 < grid[x+n][y+m] < 10:
-                            grid[x+n][y+m] = grid[x+n][y+m] + 1
+                    if 0 <= x + n < len(grid) and 0 <= y + m < len(grid[0]) and 0 < grid[x + n][y + m] < 10:
+                        grid[x + n][y + m] = grid[x + n][y + m] + 1
 
                 grid[x][y] = 0
 
-    flashes_in_step = len([(x, y) for (x, y) in AoCHelper.get_all_coordinates_in_grid(10, 10) if grid[x][y] == 0])
+    flashes_in_step = len([(x, y) for x, y in product(range(10), range(10)) if grid[x][y] == 0])
 
     if step <= 100:
         flashes += flashes_in_step
 
-    if  flashes_in_step == 100:
+    if flashes_in_step == 100:
         print(f"Finished at step {step}")
         break
 
