9fdb1f17d1877ec843f217772b4024091d052ce4
Midjord <midjord.andreas@gmail.com>
2021-12-14 08:46:26 +0100

    Day 14 - Part 2 WIP

--- Day14.py
+++ Day14.py
@@ -5,43 +5,42 @@ import time
 
 start_time = time.time()
 
-input_lines = AoCHelper.read_input_lines("day14/input1.txt")
+input_lines = AoCHelper.read_input_lines("day14/test1.txt")
 
-template = list(input_lines[0])
+template = input_lines[0]
 instructions = {}
 
 for i in range(2, len(input_lines)):
     pos, val = input_lines[i].split(' -> ')
     instructions[pos] = val
 
+changes = { pos : [pos[0] + instructions[pos], instructions[pos] + pos[1]] for pos in instructions}
+
+c = Counter()
+
+for ins in instructions:
+    c[ins] = 1 if ins in template else 0
+
 for n in range(10):
-    print(f"Running step {n}")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    insertions = []
+    c_new = Counter()
 
-    for i in range(len(template) - 1):
-        for ins in instructions:
-            if template[i] + template[i + 1] == ins:
-                insertions.append((instructions[ins], i + 1, True))
+    for ins in instructions:
+        new_occurences = 0
 
-    idx = 0
-    while any(b for val, i, b in insertions):
-        val, i, b = insertions[idx]
-        template.insert(i, val)
-        insertions[idx] = (val, i, False)
+        for change in changes:
+            if ins in changes[change]:
+                new_occurences += 1 * c[change]
 
-        for j in range(idx, len(insertions)):
-            val2, i2, b2 = insertions[j]
-            insertions[j] = (val2, i2 + 1, b2)
+        c_new[ins] += new_occurences
 
-        idx += 1
+    c = c_new
 
+    print(f"Finished round {n}")
 
-c = Counter(''.join(template))
 max_occurences = max(c.values())
 min_occurences = min(c.values())
-
 part_one_res = max_occurences - min_occurences
 print(part_one_res)
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
