adf8cc7ac70492c008ed19daf7703a93cf94b921
Midjord <midjord.andreas@gmail.com>
2021-12-27 13:31:40 +0100

    Day 17 - both parts

---
+++ Day17.py
@@ -0,0 +1,65 @@
+import math
+import time
+
+
+def get_corners(area):
+    a, b, c, d = area.split()
+    x1, x2 = map(int, str(c[2:-1]).split('..'))
+    y1, y2 = map(int, str(d[2:]).split('..'))
+
+    return x1, x2, y1, y2
+
+
+def lands_in_target_area(x_vel, y_vel, target):
+    res = False
+    x1, x2, y1, y2 = get_corners(target)
+    x = y = 0
+    traj = [(x, y)]
+
+    while x < x2 and (x_vel != 0 or y >= y1):
+        x += x_vel
+        y += y_vel
+        traj.append((x, y))
+
+        if x1 <= x <= x2 and y1 <= y <= y2:
+            res = True
+            break
+
+        x_vel -= (x_vel // abs(x_vel)) if x_vel != 0 else 0
+        y_vel -= 1
+
+    return res, traj
+
+
+start_time = time.time()
+
+# input = 'target area: x=20..30, y=-10..-5'
+input = 'target area: x=288..330, y=-96..-50'
+x1, x2, y1, y2 = get_corners(input)
+x_lower_limit = math.ceil((-1 + math.sqrt(1 + 8 * x1)) // 2)
+y_vel = y1
+traj_res = []
+valid_traj = set([])
+
+while y_vel <= 100:
+    hits = 0
+    for x_vel in range(x_lower_limit + x2):
+        res, traj = lands_in_target_area(x_vel, y_vel, input)
+        hits += int(res)
+
+        if res:
+            traj_res = traj
+            valid_traj.add(traj[1])
+
+    y_vel += 1
+
+
+part_one_res = max(b for a, b in traj_res)
+assert part_one_res == 4560
+print(f"Part 1: {part_one_res}")
+
+part_two_res = len(valid_traj)
+assert part_two_res == 3344
+print(f"Part 2: {part_two_res}")
+
+print("--- %s seconds ---" % (time.time() - start_time))
